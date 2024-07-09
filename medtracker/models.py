552e2c5d8c0a81a7a3b1b3389c93fd2acc4bc951
prathebaAT <pratheba63@gmail.com>
2024-07-09 13:10:51 +0530

    modified models

--- medtracker/models.py
+++ medtracker/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import timedelta, datetime
 
-# Create your models here.
+class Medicine(models.Model):
+    medicine_name = models.CharField(max_length=100)
+    description = models.TextField(blank = True, null = True)
+    manufacturer = models.CharField(max_length=100, blank=True, null=True)
+    dosage_form = models.CharField(max_length=50, blank=True, null=True)
+    expiration_date =models.DateField(blank=True, null=True)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.medicine_name
+    
+    def calculate_due_date():
+        pass
+
+class Dosage(models.Model):
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=20)
+    notes = models.TextField(blank=True, null=True)
+    prescribed_by = models.CharField(max_length=100, blank=True, null=True)
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    frequency = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.quantity} {self.unit} of {self.medicine.name} on {self.date} at {self.time}"
