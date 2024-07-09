0f064f05d8ba0954590fa3e8a444900ca3ccc01c
prathebaAT <pratheba63@gmail.com>
2024-07-09 13:09:57 +0530

    modified

--- med_dose_tracker/urls.py
+++ med_dose_tracker/urls.py
@@ -20,5 +20,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('medtracker.urls')),
+    path('api/' , include('medtracker.urls')),
 ]
