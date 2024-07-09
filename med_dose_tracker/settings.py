ab9c38198c09f9e8e779b8eee9d38915c3990dc5
prathebaAT <pratheba63@gmail.com>
2024-07-09 13:11:19 +0530

    modified settings

--- med_dose_tracker/settings.py
+++ med_dose_tracker/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'medtracker'
+    'rest_framework',
+    'medtracker',
 ]
 
 MIDDLEWARE = [
