77242ba738220773012f661aa2349df73c13b8a3
prathebaAT <pratheba63@gmail.com>
2024-07-09 13:10:31 +0530

    modified urls.py

--- medtracker/urls.py
+++ medtracker/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MedicineViewSet, DosageViewSet
+
+
+router = DefaultRouter()
+router.register(r'medicines', MedicineViewSet)
+router.register(r'doses', DosageViewSet)
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
+    path('', include(router.urls)),
 ]
