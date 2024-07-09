945aba2100d2b554e331eea690f93c643689c8ef
prathebaAT <pratheba63@gmail.com>
2024-07-09 13:10:15 +0530

    modified view.py

--- medtracker/views.py
+++ medtracker/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import viewsets
 
-def home_page(request):
-    return render(request, "homepage.html" ,{})
+from .models import Medicine, Dosage
+from .serializers import MedicineSerializer, DosageSerializer
+
+
+class MedicineViewSet(viewsets.ModelViewSet):
+    queryset = Medicine.objects.all()
+    serializer_class = MedicineSerializer
+
+class DosageViewSet(viewsets.ModelViewSet):
+    queryset = Dosage.objects.all()
+    serializer_class = DosageSerializer
