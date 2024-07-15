c1745b58c9202cfe2a7aca3b4d21f813aaedc848
prathebaAT <pratheba63@gmail.com>
2024-07-15 11:55:58 +0530

    modified

--- med_dose_tracker/urls.py
+++ med_dose_tracker/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/' , include('medtracker.urls')),
-]
+]+ debug_toolbar_urls()
