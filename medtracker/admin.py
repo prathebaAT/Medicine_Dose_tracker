a427e006b97a33fcc3bd21caa169e0e3d0c8a602
prathebaAT <pratheba63@gmail.com>
2024-07-27 18:46:34 +0530

    modified

--- medtracker/admin.py
+++ medtracker/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import  Medicine, Dosage
 
 
+
+
+class DosageAdmin(admin.ModelAdmin):
+    list_display = ('medicine', 'patient', 'date', 'time', 'quantity', 'unit', 'get_due_dates')
+    
+    def get_due_dates(self, obj):
+        return obj.get_due_dates()
+    get_due_dates.short_description = 'Due Dates'
+
 admin.site.register(Medicine)
-admin.site.register(Dosage)
-
-
+admin.site.register(Dosage,DosageAdmin)
--- medtracker/models.py
+++ medtracker/models.py
@@ -28,14 +28,22 @@ class Dosage(models.Model):
     notes = models.TextField(blank=True, null=True)
     prescribed_by = models.CharField(max_length=100, blank=True, null=True)
     patient = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    frequency = models.CharField(max_length=50, blank=True, null=True)
+    frequency = models.IntegerField()
 
     def __str__(self):
         return f"{self.patient} added {self.quantity} {self.unit} of {self.medicine.medicine_name} on {self.date} at {self.time}"
 
     def calculate_due_date(self):
-        frequency_per_day = int(self.frequency)
-        depletion_days = self.medicine.stock / frequency_per_day
-        depletion_date = self.date + timedelta(days=depletion_days)
-        reminder_date = depletion_date - timedelta(days=3)
-        return depletion_date, reminder_date
+        try:
+            frequency_per_day = int(self.frequency)
+            depletion_days = self.medicine.stock // frequency_per_day
+            depletion_date = self.date + timedelta(days=depletion_days)
+            reminder_date = depletion_date - timedelta(days=3)
+            return f"Depletion Date: {depletion_date.strftime('%Y-%m-%d')}", f"Reminder Date: {reminder_date.strftime('%Y-%m-%d')}"
+        except ValueError:
+            return "Invalid frequency value", "Invalid frequency value"
+
+
+    def get_due_dates(self):
+        depletion_date, reminder_date = self.calculate_due_date()
+        return f"Due: {depletion_date}, Reminder: {reminder_date}"
